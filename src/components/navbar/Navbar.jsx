c31523f47302fc9518265ba8617333584629ddbf
mariumyounus <mariumyounus4@gmail.com>
2024-08-25 00:41:06 +0100

    Links complete

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import { Londrina_Sketch } from 'next/font/google'
+import Link from 'next/link'
+import React from 'react'
+import Links from './links/Links'
+
+const Navbar = () => {
+  return (
+    <div>
+     <div>Logo</div>
+     <div>
+     <Links/>
+
+     </div>
+    </div>
+  )
+}
+
+export default Navbar
